a70b424140493e1e389d69f40534ee2abeb6602e
Anatolii Mikhailov <aa.mikhaylov.junior@gmail.com>
2025-11-28 15:11:52 +0300

    Initial commit: my WorkAdventure map

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
+<<<<<<< HEAD
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 let currentPopup: any | undefined;
@@ -43,5 +44,32 @@ WA.onInit()
         bootstrapExtra().catch(() => {});
     })
     .catch(() => {});
+=======
+console.log("Script started");
+
+WA.onInit()
+    .then(() => {
+        console.log("Scripting API ready");
+
+        const areaName = "ZoneHouse1";       // имя объекта-зоны в Tiled
+        const wallsLayerName = "InvisibleWalls"; // имя Tile Layer с крышей / стенами
+
+        // На всякий случай убедимся, что слой включён при старте
+        WA.room.showLayer(wallsLayerName);
+
+        // Зашли в зону — прячем слой с крышей
+        WA.room.area.onEnter(areaName).subscribe(() => {
+            console.log("Enter area:", areaName);
+            WA.room.hideLayer(wallsLayerName);
+        });
+
+        // Вышли из зоны — возвращаем слой
+        WA.room.area.onLeave(areaName).subscribe(() => {
+            console.log("Leave area:", areaName);
+            WA.room.showLayer(wallsLayerName);
+        });
+    })
+    .catch((e) => console.error(e));
+>>>>>>> b25b2e0 (Initial commit: my WorkAdventure map)
 
 export {};
