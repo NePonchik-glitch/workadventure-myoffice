8f6858e9bdd75456e953fbca6d12d74bad44b6fc
Anatolii Mikhailov <aa.mikhaylov.junior@gmail.com>
2025-12-03 03:38:03 +0300

    Update map from local

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,22 @@ WA.onInit()
     .then(() => {
         console.log("Scripting API ready");
 
-        const areaName = "ZoneHouse1";       // имя объекта-зоны в Tiled
-        const wallsLayerName = "InvisibleWalls"; // имя Tile Layer с крышей / стенами
+        const ZoneHouse1 = "ZoneHouse1";       // имя объекта-зоны в Tiled
+        const InvisibleWalls = "InvisibleWalls"; // имя Tile Layer с крышей / стенами
 
         // На всякий случай убедимся, что слой включён при старте
-        WA.room.showLayer(wallsLayerName);
+        WA.room.showLayer(InvisibleWalls);
 
         // Зашли в зону — прячем слой с крышей
-        WA.room.area.onEnter(areaName).subscribe(() => {
-            console.log("Enter area:", areaName);
-            WA.room.hideLayer(wallsLayerName);
+        WA.room.area.onEnter(ZoneHouse1).subscribe(() => {
+            console.log("Enter area:", ZoneHouse1);
+            WA.room.hideLayer(InvisibleWalls);
         });
 
         // Вышли из зоны — возвращаем слой
-        WA.room.area.onLeave(areaName).subscribe(() => {
-            console.log("Leave area:", areaName);
-            WA.room.showLayer(wallsLayerName);
+        WA.room.area.onLeave(ZoneHouse1).subscribe(() => {
+            console.log("Leave area:", ZoneHouse1);
+            WA.room.showLayer(InvisibleWalls);
         });
     })
     .catch((e) => console.error(e));
